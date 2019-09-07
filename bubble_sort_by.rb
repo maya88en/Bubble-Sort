7a510626e93e618798700b143b5151ddb993b37c
addod19 <addodaniellarbi@gmail.com>
2019-09-07 10:14:48 +0000

    resolved merge conflict

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -22,3 +22,24 @@ def bubble_sort_by(arr)
   greetings = %w[hi hello hey]
   
   print bubble_sort_by(greetings) { |left, right| left.length - right.length }
+
+
+  def bubble_sort arr
+    n=arr.length
+
+    loop do
+        swapped=false
+        (n-1).times do |i|
+            if arr[i] > arr[i+1]
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                swapped=true
+            end
+        end
+        break if not swapped
+    end
+    arr
+end
+
+array = [7,2,7,5,1,0,4,8]
+
+p bubble_sort array
